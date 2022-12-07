5e8d3e75e153f899dae919ef87fe699dcd7ca031
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-12-07 17:40:50 +0530

    update url issue

--- frontend/src/components/Forms.jsx
+++ frontend/src/components/Forms.jsx
@@ -48,12 +48,12 @@ const Forms = () => {
         console.log(days);
        axios({
          method: "post",
-         url: "http://localhost:3070/volunteer/register",
+         url: "https://tach-for-india-assignment.vercel.app/volunteer/register",
          data: loginData,
        })
          .then((res) => {
-             console.log(res);
-             alert("form registered")
+           console.log(res);
+           alert("form registered");
          })
          .catch((err) => {
            console.log(err);
